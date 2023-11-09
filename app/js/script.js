c9e831ce37d6e9c78fefa92049894a9763100f16
munevver <mozhan@backupswarm.com>
2023-11-09 16:42:24 +0300

    changed code to handle prefs change the same as button click when the page is loaded.

--- app/js/script.js
+++ app/js/script.js
@@ -1,49 +1,40 @@
 const darkButton = document.getElementById('dark');
 const lightButton = document.getElementById('light');
 
-const setColorMode = () => {
-    if (localStorage.getItem('colorMode') == 'dark') {
-        setDarkMode();
-        darkButton.checked = true;
-    } else if (localStorage.getItem('colorMode') == 'light') {
-        setLightMode();
-        lightButton.checked = true;
-    }
-}
-const checkMode = () => {
-    if (localStorage.getItem('colorMode') == null) {
-        if (window.matchMedia("(prefers-color-scheme:light)").matches) {
-            lightButton.checked = true;
-        } else if (window.matchMedia("(prefers-color-scheme:dark)").matches) {
-            darkButton.checked = true;
-        }
-    }
-
-}
-const checkModeChange = () => {
-    window.matchMedia("(prefers-color-scheme:light)").addEventListener('change', event => {
-        checkMode();
-    })
-}
-
 const setLightMode = () => {
     document.querySelector('body').classList = 'light';
-}
+    localStorage.setItem('colorMode', 'light');
+};
+
 const setDarkMode = () => {
     document.querySelector('body').classList = 'dark';
+    localStorage.setItem('colorMode', 'dark');
+};
+
+const colorModeFromLocalStorage = () => {
+    return localStorage.getItem('colorMode');
 }
-setColorMode();
-checkModeChange();
-const radioButtons = document.querySelectorAll('.toggle__wrapper input');
-for (let i = 0; i < radioButtons.length; i++) {
-    radioButtons[i].addEventListener('click', (event) => {
-        if (darkButton.checked) {
-            localStorage.setItem('colorMode', 'dark');
-            setDarkMode();
-        } else {
-            localStorage.setItem('colorMode', 'light');
-            setLightMode();
-        }
-    })
+const colorModeFromPreferences = () => {
+    return window.matchMedia("(prefers-color-scheme:dark)").matches
+        ? 'dark'
+        : 'light';
 }
 
+const loadAndUpdateColor = () => {
+    const color = colorModeFromLocalStorage() || colorModeFromPreferences();
+    color == 'dark' ? darkButton.click() : lightButton.click();
+};
+
+const radioButtons = document.querySelectorAll('.toggle__wrapper input');
+radioButtons.forEach(button => {
+    button.addEventListener('change', event => {
+        darkButton.checked ? setDarkMode() : setLightMode();
+    });
+});
+
+
+window.matchMedia("(prefers-color-scheme:dark)").addEventListener('change', (event) => {
+    event.matches ? darkButton.click() : lightButton.click();
+});
+
+loadAndUpdateColor();
